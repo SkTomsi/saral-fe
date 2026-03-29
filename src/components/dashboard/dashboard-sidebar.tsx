117ee46468a2490e6b6df1997e1932a6e4f3d689
Tomcy <designsbytomc@gmail.com>
2026-03-29 16:30:02 +0530

    feat: shadcn sidebar

--- src/components/dashboard/dashboard-sidebar.tsx
+++ src/components/dashboard/dashboard-sidebar.tsx
@@ -1,10 +1,18 @@
-import { Brain, Briefcase, ClipboardList, Home, Wallet } from "lucide-react";
+import {
+	Brain,
+	Briefcase,
+	ClipboardList,
+	Home,
+	Settings,
+	Wallet,
+} from "lucide-react";
 import type * as React from "react";
 import { NavLink } from "react-router";
 
 import {
 	Sidebar,
 	SidebarContent,
+	SidebarFooter,
 	SidebarHeader,
 	SidebarMenu,
 	SidebarMenuButton,
@@ -44,14 +52,14 @@ const navItems = [
 export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
 	return (
 		<Sidebar {...props}>
-			<SidebarContent className="p-4 bg-magenta-3">
-				<SidebarHeader>
-					<div className="flex items-center gap-4">
-						<div className="size-12 rounded-xl bg-magenta-12" />
-						<HeaderLSemiBold>SARAL OS</HeaderLSemiBold>
-					</div>
-				</SidebarHeader>
-				<SidebarMenu className="mt-6 gap-2">
+			<SidebarHeader className="bg-magenta-3">
+				<div className="flex items-center gap-4">
+					<div className="size-12 rounded-xl bg-magenta-12" />
+					<HeaderLSemiBold>SARAL OS</HeaderLSemiBold>
+				</div>
+			</SidebarHeader>
+			<SidebarContent className="bg-magenta-3">
+				<SidebarMenu className="gap-1 px-4 mt-6">
 					{navItems.map((item) => (
 						<SidebarMenuItem key={item.title} className="[&_svg]:size-6 ">
 							<SidebarMenuButton
@@ -68,7 +76,22 @@ export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
 					))}
 				</SidebarMenu>
 			</SidebarContent>
-
+			<SidebarFooter className="bg-magenta-3">
+				<SidebarMenu>
+					<SidebarMenuItem key={"/settings"} className="[&_svg]:size-6 ">
+						<SidebarMenuButton
+							asChild
+							className="bg-magenta-3 hover:bg-magenta-1 text-brand-secondary hover:text-magenta-12 flex items-center rounded-[10px] gap-2 p-3 h-10 transition-all duration-300 ease-out"
+						>
+							{/* TODO: Add active state */}
+							<NavLink to={"/settings"}>
+								<Settings />
+								<span>Settings</span>
+							</NavLink>
+						</SidebarMenuButton>
+					</SidebarMenuItem>
+				</SidebarMenu>
+			</SidebarFooter>
 			<SidebarRail />
 		</Sidebar>
 	);
